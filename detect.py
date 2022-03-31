10fc33b3cdf572b08c2e325be65905887ac8f4a4
znypr <sandrooo126@gmail.com>
2022-03-31 21:47:34 +0200

    contour tests, tried different filters

--- detect.py
+++ detect.py
@@ -1,25 +1,48 @@
 import cv2
+import numpy as np
 
-img_path = r"./images_maze/maze2.jpg"
+path = "./images/maz3.jpg"
 
-def read_img(path):
+def scale(img, p):
 
-    img = cv2.imread(path, 0)
-    
-    return img
+    width = int(img.shape[1] * p)
+    height = int(img.shape[0] * p)
+
+    dsize = (width, height)
+
+    return cv2.resize(img, dsize)
+
+def filter(img):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(11,11),0)
+
+    # binary
+    bi = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+
+    # canny
+    img_canny = cv2.Canny(img,100,300)
+
+    # sobel
+    img_sobelx = cv2.Sobel(blur,cv2.CV_8U,1,0,ksize=5)
+    img_sobely = cv2.Sobel(blur,cv2.CV_8U,0,1,ksize=5)
+    img_sobel = img_sobelx + img_sobely
 
 
+    # prewitt
+    kernelx = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
+    kernely = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+    img_prewittx = cv2.filter2D(blur, -1, kernelx)
+    img_prewitty = cv2.filter2D(blur, -1, kernely)
 
+    return [img, blur, bi, img_canny, img_sobelx, img_sobely, img_sobel, img_prewittx, img_prewitty, img_prewittx+img_prewitty]
 
-if __name__ == '__main__':
+if __name__=="__main__":
 
-    img = cv2.imread(img_path,1)
+    img = cv2.imread(path, 1)
 
-    edged = cv2.Canny(img, 30, 200)
+    arr = filter(img)
 
+    for i in range(len(arr)):
+        cv2.imwrite("./output/"+str(i)+".jpg", scale(arr[i], 0.5))
 
-    contours, hierarchy = cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-
-    edged = cv2.resize(edged, (1920,1080))
-    cv2.imshow("maze", edged)
-    cv2.waitKey(0)
