3b76ff9e368a5abccc22b9525321898d1b21fdeb
Znypr <sandrooo126@gmail.com>
2022-06-15 17:38:49 +0200

    minor refactor of main

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-import maze_detection.detect as d
-import controller.control as c
-import visualizer.visualize as v
-import solver.solve as s
-import solver.entities as nt
 import sys
 import os
 import csv
 import math
 
+import maze_detection.detect as d
+import controller.control as c
+import visualizer.visualize as v
+import solver.solve as s
+import solver.entities as nt
+
+
 if __name__ == "__main__":
 
-    c.establish_connection()
+    # Connection
+    #c.establish_connection()
 
     # Detection
-    # maze, pos = d.detect()
-
-    # Solver
-    # actions = s.solve(maze, pos)
+    maze = d.detect_maze('media\images\maz3.jpg', False)
+    agent = nt.Agent(2, 0, 180) # x,y,rotation
 
     # Visualize
-    # v.visualize(maze, pos, actions)
-
-    maze = d.detect_lab('media\images\maz3.jpg', False)
-    agent = nt.Agent(2, 0, 180)
-
-    v.visualize(maze, agent)
+    #v.visualize(maze, agent)
     exits = s.find_exits(maze)
 
+    # Learn
     s.set_lab_weights(maze, exits)
-    s.escape_maze(maze, agent)
+    #s.escape_maze(maze, agent)
