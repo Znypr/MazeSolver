828851dda2148b9ddecc9ba4316049669ceb7e3a
Znypr <sandrooo126@gmail.com>
2022-06-18 21:31:19 +0200

    added arguments; works with all imgs in input

--- main/run.py
+++ main/run.py
@@ -2,6 +2,8 @@ import sys
 import os
 import csv
 import math
+import glob
+import argparse
 
 import detector.detect as d
 import controller.control as c
@@ -11,22 +13,35 @@ import solver.entities as nt
 
 def main():
 
-    simulate = True
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Solve a maze')
+    parser.add_argument('-c', '--control', help='flag if robot should be controlled', action='store_true')
+    parser.add_argument('-v', '--visualize', help='flag if detection steps should be visualized', action='store_true')
+    args = parser.parse_args()
 
     # Connection
-    c.establish_connection() if not simulate else None
+    c.establish_connection() if args.control else None
 
-    # Detection
-    maze = d.detect_maze('input/maz3.jpg', False) # flag for visualizing images
-    agent = nt.Agent(2, 0, 180) # x,y,rotation
+    for maze in glob.glob('input/*.jpg'):
 
-    # Visualize
-    v.visualize(maze, agent)
-    exits = s.find_exits(maze)
+        print('\n     Current Maze to be solved: {}\n\n'.format(maze))
 
-    # Learn
-    s.set_lab_weights(maze, exits)
-    s.escape_maze(maze, agent, simulate)
+        # Detection
+        maze = d.detect_maze(maze, args.visualize) # flag for visualizing images
+        agent = nt.Agent(2, 0, 180) # x,y,rotation
+
+        # Visualize
+        v.visualize(maze, agent)
+        exits = s.find_exits(maze)
+
+        # Learn
+        s.set_lab_weights(maze, exits)
+        s.escape_maze(maze, agent, args.control)
+
+        if  input('Do you want to continue? (y/n)') == 'n':
+            break
+    
+    print('\nAll mazes have been solved!')
 
 if __name__ == "__main__":
 
